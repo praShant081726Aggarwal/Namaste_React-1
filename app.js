587aafa16dd1e7198013176a2a1c4d313397517f
unknown <aggarwalprashant235@gmail.com>
2023-11-21 10:44:55 +0530

    Episode-1

---
+++ app.js
@@ -0,0 +1,14 @@
+// const heading = React.createElement("h1", {id: "heading"}, "Hello world from React")
+const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(heading)
+// console.log(heading)
+
+const parent = React.createElement("div",
+    { id: "parent" },
+    React.createElement("div",
+        { id: "child" },
+        [React.createElement("h1", {}, "I'm a h1 tag"), React.createElement("h2", {}, "I'm a h2 tag")])
+)
+
+
+root.render(parent)
