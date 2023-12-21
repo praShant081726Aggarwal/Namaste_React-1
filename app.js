a9deab80bce855963840b28923bea25aabc6849c
unknown <aggarwalprashant235@gmail.com>
2023-12-21 13:38:09 +0530

    jsx and React Component

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-// const heading = React.createElement("h1", {id: "heading"}, "Hello world from React")
-const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(heading)
-// console.log(heading)
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",
-    { id: "parent" },
-    React.createElement("div",
-        { id: "child" },
-        [React.createElement("h1", {}, "I'm a React tag"), React.createElement("h2", {}, "I'ce changed CDN to React ")])
+
+const heading = (
+    <h1 id="heading">
+    <h2>My name is Antony Gonsalviz</h2>
+  </h1>
+);
+
+// React component.
+const HeadingComponennt =()=>{
+    return <h1>
+        Prashant Aggarwal will be a superhero
+    </h1>
+}
+
+const Component1 = ()=>{
+    return <div className="Title">
+        <h1>
+            Hi, I'm Prashant Aggarwal
+        </h1>
+    </div>
+}
+// Inserting a component into other component is known as component composition.
+const Component2 = ()=>( // if we use () then we don't need to write return..
+    <div>
+        <Component1/>  
+        <h2>
+            I'll be a great Engineer......🚀
+        </h2>
+    </div>
 )
 
-
-root.render(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Component2/>);
