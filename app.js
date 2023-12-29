57a62f06825ed7bd9bedc0fe959c0716840bdead
unknown <aggarwalprashant235@gmail.com>
2023-12-29 11:03:37 +0530

    new update

--- app.js
+++ app.js
@@ -1,36 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+/**
+ * Header --> Logo, Nav items,
+ * Body --> Search, Restrarant container, Restaurant card
+ * Footer --> copyright, links , address, contact
+ */
 
-const heading = (
-    <h1 id="heading">
-    <h2>My name is Antony Gonsalviz</h2>
-  </h1>
-);
-
-// React component.
-const HeadingComponennt =()=>{
-    return <h1>
-        Prashant Aggarwal will be a superhero
-    </h1>
+// Header components and Header  
+const Search =()=>{
+    return <div className="search">Search</div>
 }
-
-const Component1 = ()=>{
-    return <div className="Title">
-        <h1>
-            Hi, I'm Prashant Aggarwal
-        </h1>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.shutterstock.com/image-vector/sweet-cake-logo-cupcake-icon-600nw-1575517294.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li><Search/></li>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-}
-// Inserting a component into other component is known as component composition.
-const Component2 = ()=>( // if we use () then we don't need to write return..
-    <div>
-        <Component1/>  
-        <h2>
-            I'll be a great Engineer......🚀
-        </h2>
-    </div>
-)
+  );
+};
 
+// Body components and Body
+
+
+const RestaurantCard = (props) => {
+    return <div className="res-card"> 
+        <img 
+        className="res-logo" alt="res-logo"
+        src="https://b.zmtcdn.com/data/dish_photos/993/6788af1e267ee17cd67d818208e2a993.jpg?output-format=webp"/>
+        <h3>zomato</h3>
+    </div>
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="res-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Component2/>);
+root.render(<AppLayout />);
